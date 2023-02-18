d4ff4f1afcddf4ec09907d22a88d4e7e8574a4e2
MaxH403 <114328463+MaxH403@users.noreply.github.com>
2023-02-17 16:54:09 -0800

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,16 @@
+/** --------------------------------------------------------
+* @project    Asteroids (constants.h)
+* @author  Maxwell Hermens
+* @version 3.0
+*
+* Demonstrates:
+*   Draws multiple asteroids, detects collision between ship and
+* asteroids and creats a explosion effect.
+* ------------------------------------------------------------ */
+#pragma once
+
+//Defines global constants
+const int SCREEN_WIDTH = 400;
+const int SCREEN_HEIGHT = 400;
+const float PI = 3.1415;
+const int MAX_ASTEROIDS = 20;
